df807488502abc27060eeb03d230db988c43ab67
Oleg Kanatov <olegkanatov7@gmail.com>
2021-08-05 12:40:06 +0300

    Add all my files

--- RocketLaunch/ViewController.swift
+++ RocketLaunch/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var launchButton = UIButton(type: .system)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        launchButtonSetup()
     }
 
-
+    func launchButtonSetup() {
+        launchButton.setImage(UIImage(named: "powerIcon"), for: .normal)
+        launchButton.tintColor = .black
+        
+        launchButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(launchButton)
+        
+        
+        NSLayoutConstraint.activate([
+            launchButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            launchButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            launchButton.heightAnchor.constraint(equalToConstant: 120),
+            launchButton.widthAnchor.constraint(equalToConstant: 120)
+        ])
+    }
+    
 }
 
